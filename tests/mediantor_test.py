a78423809c6e61e3c104c8ca79a420452d90f228
SoVictor <sovictorcom@yandex.ru>
2022-03-20 00:53:46 +0700

    Add tests

--- tests/mediantor_test.py
+++ tests/mediantor_test.py
@@ -1,4 +1,5 @@
 from mediantor import IMediantor, Mediantor, make_mediantor
+import os
 import pytest
 
 
@@ -21,3 +22,27 @@ def test_trivial(mediantor_type: Mediantor):
     # values: 1, 2, 3, 4
 
     assert mediantor.take() == 2
+
+
+@pytest.mark.parametrize('mediantor_type', MEDIANTORS_LIST)
+def test_on_data(mediantor_type: Mediantor):
+    for input_filename in os.listdir("tests/data"):
+        if not input_filename.endswith(".in"):
+            continue
+
+        input_path: str = "tests/data/" + input_filename
+        ans_path: str = input_path[:-2] + "out"
+        with open(input_path, "r") as input_file, open(ans_path, "r") as ans_file:
+            n = int(input_file.readline())
+
+            mediantor: IMediantor = make_mediantor(mediantor_type, n)
+
+            for _ in range(n):
+                numbers: list[int] = list(map(int, input_file.readline().split()))
+                operation: int = numbers[0]
+                if operation == 1:
+                    x: int = numbers[1]
+                    mediantor.insert(x)
+                else:
+                    x: int = int(ans_file.readline())
+                    assert x == mediantor.take()
