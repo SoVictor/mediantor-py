3b5383f3ab05289308ce01020ad444e1251d7063
SoVictor <sovictorcom@yandex.ru>
2022-03-19 23:09:16 +0700

    Parametrize trivial test

--- tests/mediantor_test.py
+++ tests/mediantor_test.py
@@ -1,8 +1,13 @@
-from mediantor import IMediantor, MediantorHeap
+from mediantor import IMediantor, Mediantor, make_mediantor
+import pytest
 
 
-def test_trivial():
-    mediantor: IMediantor = MediantorHeap()
+MEDIANTORS_LIST: list[Mediantor] = [Mediantor.HEAP, Mediantor.SQRT_DECOMP, Mediantor.SORTED_LIST]
+
+
+@pytest.mark.parametrize('mediantor_type', MEDIANTORS_LIST)
+def test_trivial(mediantor_type: Mediantor):
+    mediantor: IMediantor = make_mediantor(mediantor_type, 4)
 
     mediantor.insert(1)
     mediantor.insert(2)
