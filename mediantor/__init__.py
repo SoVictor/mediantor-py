c6659a3baa4c971fe8679cf9459a5c5c5717a909
SoVictor <sovictorcom@yandex.ru>
2022-03-21 23:46:33 +0700

    Add flake8 to github actions

--- mediantor/__init__.py
+++ mediantor/__init__.py
@@ -3,3 +3,5 @@ from .mediantor import make_mediantor, Mediantor
 from .mediantor_heap import MediantorHeap
 from .mediantor_sorted_list import MediantorSortedList
 from .mediantor_sqrt_decomp import MediantorSqrtDecomp
+
+__all__ = ["IMediantor", "make_mediantor", "Mediantor", "MediantorHeap", "MediantorSortedList", "MediantorSqrtDecomp"]
